bbb2b953800eb14bfde164b25ab47a68e03e210f
chandrakishor <chandrakishoret197@gmail.com>
2024-09-27 19:30:39 +0530

    Work Completed

--- src/Component/Task/Task1.jsx
+++ src/Component/Task/Task1.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import '../Task/style.css'
+import '../Task/style.css';
+
+
+const cardData = [
+    { id: 1, title: "Card 1", text: "Some text for card 1" },
+    { id: 2, title: "Card 2", text: "Some text for card 2" },
+    { id: 3, title: "Card 3", text: "Some text for card 3" },
+    { id: 4, title: "Card 4", text: "Some text for card 4" },
+    { id: 5, title: "Card 5", text: "Some text for card 5" },
+    { id: 6, title: "Card 6", text: "Some text for card 6" }
+];
 
 function Task1() {
     return (
         <>
+        <div className='main'>
             <div className="container text-center">
                 <div className="row">
-                    <div className="col">
-                        <Card />
-                    </div>
-                    <div className="col ">
-                        Column2
-                    </div>
-                    <div className="col">
-                        Column3
-                    </div>
+                {cardData.map((card) => (
+                            <div className="col-12 col-sm-6 col-md-4 mb-4" key={card.id}>
+                                <Card title={card.title} text={card.text} />
+                            </div>
+                        ))}
+
                 </div>
+            </div>
+
             </div>
         </>
     );
@@ -23,18 +33,17 @@ function Task1() {
 
 export default Task1;
 
- function Card(){
+ function Card({ title, text}){
     return(
      
-        <div className="card color" style={{ width: '18rem' }}>
+        <div className="card color" style={{ width: '100%' }}>
                             {/* Replace "..." with a valid image source */}
-                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Placeholder" />
+                            <div className='img-con'></div>
+                            <img src="https://img.freepik.com/free-vector/hand-coding-concept-illustration_114360-8113.jpg?w=740&t=st=1727443904~exp=1727444504~hmac=152e360476eb5011c0fb88468d1f145d353139ba1791aff11727ab41ddaef8e3" className="card-img-top" alt="Placeholder" />
                             <div className="card-body">
                                 <div className='del-card'>X</div>
-                                <h2 className="card-title ">Heading Section </h2>
-                                <p className="card-text ptext">
-                                    Some quick example text to build on the card title t.
-                                </p>
+                                <h2 className="card-title ">{ title} </h2>
+                                <p className="card-text ptext">{text}</p>
                                 <p className="card-text">Mon, 27 Sep 2024 14:57 GMT </p>
                             </div>
                         </div>
